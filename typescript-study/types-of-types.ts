764e4e42f66d9421a31cf26b0a4e7db859ef98a3
Eastyn <eastynwork@gmail.com>
2024-06-21 18:49:45 +0900

    learns about type void, unknown and never

--- typescript-study/types-of-types.ts
+++ typescript-study/types-of-types.ts
@@ -37,3 +37,38 @@ const exArr: [string, number, boolean] = ["nuri", 3, true];
 
 let d: undefined = undefined;
 let e: null = null;
+
+/*
+    void
+        for function that don't return anything
+        you don't have to specify void function
+
+    unknown
+        unknown is used when we don't know the type of a variable in advance
+
+    never
+        never ever return a value or run a code
+        but it can throw an error
+*/
+
+let a: unknown;
+
+if (typeof a === "number") {
+	let b = a + 1;
+}
+
+function hello(): void {
+	console.log("hello");
+}
+
+function never(name: string | number) {
+	// return "X"
+	// throw new Error("xxx") available
+	if (typeof name === "string") {
+		name;
+	} else if (typeof name === "number") {
+		name;
+	} else {
+		name; // this one is type never which code should never run
+	}
+}
