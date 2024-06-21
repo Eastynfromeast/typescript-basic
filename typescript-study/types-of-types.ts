48fd50e5600c7aaccc409a075a363d9a256c0b72
Eastyn <eastynwork@gmail.com>
2024-06-21 18:40:57 +0900

    learn about readonly, readonly tuple and any

--- typescript-study/types-of-types.ts
+++ typescript-study/types-of-types.ts
@@ -3,19 +3,37 @@
     ? means it can be undefined
 
     How you can specify the return type of a function 
-*/
+    function fnName(arg:type):type{}
 
-let a: number[] = [1, 2];
+    readonly
+        just put "readonly" before the type you want not to insert the value
+
+    any is the type when you use to escape from TypeScript
+        suggest not to use any to be in the protection from TS
+*/
 
 type Age = number;
 
 type Player = {
-	name: string;
+	readonly name: string;
 	age?: Age;
 };
 
-function playerMaker(name: string): Player {
+const playerMaker = (name: string): Player => ({ name });
+/* function playerMaker(name: string): Player {
 	return {
 		name,
 	};
-}
+} */
+
+const testPlayer = playerMaker("nico");
+testPlayer.age = 1;
+
+const numberArr: readonly number[] = [1, 2, 3, 4, 5];
+// numberArr.push(0)
+
+const exArr: [string, number, boolean] = ["nuri", 3, true];
+// set minimum amount of elements
+
+let d: undefined = undefined;
+let e: null = null;
