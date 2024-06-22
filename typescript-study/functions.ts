4c8f5214de8cab3217312f0ef72115b8e518b7ad
Eastyn <eastynwork@gmail.com>
2024-06-23 00:27:34 +0900

    official TS docs is not good to catch the concept in a glance

--- typescript-study/functions.ts
+++ typescript-study/functions.ts
@@ -60,15 +60,47 @@ add(1,2,3)
     # Generic 
         is kind of placeholder for your type
         and it is generating signature calls on demand for us
+        '제네릭은 선언 시점이 아니라 생성 시점에 타입을 명시하여 하나의 타입만이 아닌 다양한 타입을 사용할 수 있도록 하는 기법이다.'
 
+        It is always better to let TS infer the type
 */
-
+/* 
 type SuperPrint = <T, V>(a: T[], b: V) => T;
 // let TS knows this call signature is going to receive generics
 
-const superPrint: SuperPrint = arr => arr[0];
+const superPrint: SuperPrint = arr => arr[0]; */
 
-superPrint([1, 2, 3, 4], "x");
-superPrint([true, false, true], 1);
-superPrint(["a", "b", "c"], false);
-superPrint([1, 2, true, false, "hello"], "bye"); // No overload matches this call
+function superPrint<T>(a: T[]) {
+	return a[0];
+}
+
+superPrint([1, 2, 3, 4]);
+superPrint([true, false, true]);
+superPrint(["a", "b", "c"]);
+superPrint([1, 2, true, false, "hello"]); // No overload matches this call
+
+type Player2<E> = {
+	name: string;
+	extraInfo: E;
+};
+
+type JaneExtra = {
+	favFood: string;
+};
+type JanePlayer = Player2<JaneExtra>;
+
+const jane: JanePlayer = {
+	name: "jane",
+	extraInfo: {
+		favFood: "Kimchi",
+	},
+};
+
+const john: Player2<null> = {
+	name: "john",
+	extraInfo: null,
+};
+
+type arrNumber = Array<number>;
+
+let arrA: arrNumber = [1, 2, 3, 4];
